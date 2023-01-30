795eefee016add3c2b18001e4a2342a38c44b0f4
Fernando Magaton <104094324+fernandoagmagaton@users.noreply.github.com>
2023-01-30 15:16:28 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<title>Teste da Linguagem PHP</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php 
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Fernando Magaton<br>';
+				echo 'Linkedin: linkedin.com/in/fernando-magaton-311537238<br>';
+				echo 'Twitch: twitch.tv/mimiau_200<br>';
+				echo 'Facebook: facebook.com/FeMagaton<br>';
+				echo 'Github: github.com/fernandoagmagaton<br>';
+				echo 'Instagram: instagram.com/fmagaton<br>';
+				echo 'YouTube: youtube.com/@magons1273<br>'; 
+				echo 'Discord: discord.gg/6E3N26NJ<br>'; 
+
+			?>
+		</body>
+	</html>
