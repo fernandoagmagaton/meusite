cc9ec3ec3d6dc74da5a60ab545dc9024790c20cd
Fernando Magaton <104094324+fernandoagmagaton@users.noreply.github.com>
2023-01-30 15:23:19 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -8,13 +8,13 @@
 			<?php 
 				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
 				echo 'Autor: Fernando Magaton<br>';
-				echo 'Linkedin: linkedin.com/in/fernando-magaton-311537238<br>';
-				echo 'Twitch: twitch.tv/mimiau_200<br>';
-				echo 'Facebook: facebook.com/FeMagaton<br>';
-				echo 'Github: github.com/fernandoagmagaton<br>';
-				echo 'Instagram: instagram.com/fmagaton<br>';
-				echo 'YouTube: youtube.com/@magons1273<br>'; 
-				echo 'Discord: discord.gg/6E3N26NJ<br>'; 
+				echo 'Linkedin: https://www.linkedin.com/in/fernando-magaton-311537238/<br>';
+				echo 'Twitch: https://www.twitch.tv/mimiau_200<br>';
+				echo 'Facebook: https://www.facebook.com/FeMagaton<br>';
+				echo 'Github: https://github.com/fernandoagmagaton<br>';
+				echo 'Instagram: https://www.instagram.com/fmagaton<br>';
+				echo 'YouTube: https://youtube.com/@magons1273<br>'; 
+				echo 'Discord: https://discord.gg/6E3N26NJ<br>'; 
 
 			?>
 		</body>
